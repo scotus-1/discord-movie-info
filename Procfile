edfc60861d3eabd46cd0d5c1bf2041858cc6f934
Titus Nguyen <titusstar007@gmail.com>
2021-02-16 17:33:03 -0700

    that didnt work last time

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: gunicorn flaskwebhookresponse:app
+web: gunicorn wsgi:app
 
---
+++ app/flaskwebhookresponse.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, jsonify, render_template
+from discord_interactions import verify_key_decorator
+
+CLIENT_PUBLIC_KEY = "d164a6f844f2d9b2026508f3e826aeac830b8131ab95d22957f681d4d15b555f"
+app = Flask(__name__, template_folder="templates")
+
+@app.route('/interactions', methods=['POST'])
+@verify_key_decorator(CLIENT_PUBLIC_KEY)
+def pong():
+    if request.json["type"] == 1:
+        return jsonify({
+            "type": 1
+        })
+
+@app.route('/')
+def return_html():
+    return render_template("home.html")
+
